6ed4f26c6f2e4489ca876f3d5e7afb1f542d1c47
AKashton <ashtonrcurry@hotmail.com>
2023-11-11 17:49:29 -0900

    making server file edits, not done

--- server.py
+++ server.py
@@ -1,9 +1,29 @@
 import socket
 import threading
+import json
+import threading
+import time
 from blockchain import Block, Blockchain
+
+#blockchain initialization
+blockchain = Blockchain()
+
+#this function deals with the client sending messages to the server
 def handle_client(connection, address):
     print(f"New connection from {address}")
     # Handle client connection and messages here
+    while True:
+        try:
+            message = connection.recv(1024).decode()
+            if message:
+                #add out message to blockchain
+                new_block = Block(time.time(), message)
+                blockchain.add_block(new_block)
+                broadcast_blockchain(all_connections)
+            else:
+                break
+        except:
+            continue
     connection.close()
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
