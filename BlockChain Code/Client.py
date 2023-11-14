0216b1321255da6fda4d6407217980c0c44e2be5
acondor123 <chkreis400@gmail.com>
2023-11-14 14:44:28 -0900

    p2p chat

--- BlockChain Code/Client.py
+++ BlockChain Code/Client.py
@@ -1,8 +1,27 @@
 import socket
-from blockchain import Block, Blockchain
-client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.connect(('localhost', 5555))
+import threading
 
-# Send and receive messages here
+def receive_messages(client_socket):
+    while True:
+        data, address = client_socket.recvfrom(1024)
+        print(f"Received: {data.decode()}\n> ")
 
-client.close()
+# Peer 2 (acting as client)
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+server_address = ("localhost", 55555)
+
+client_socket.sendto("test".encode(), server_address)
+print(f"Peer 2 (Client) connected to {server_address[0]}:{server_address[1]}")
+
+data = client_socket.recv(1024).decode()
+ip, sport, dport = data.split(" ")
+print(data)
+
+# Start a thread to receive messages
+receive_thread = threading.Thread(target=receive_messages, args=(client_socket,))
+receive_thread.start()
+
+# Send messages
+while True:
+    message = input("> ")
+    client_socket.sendto(message.encode(), (ip, int(sport)))
